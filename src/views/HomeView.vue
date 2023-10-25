505de6b78dd239d3ea19c36daad3948026ba5bce
Stella Maia <stellamaia97@hotmail.com>
2023-10-25 17:29:50 -0300

    Implementação da HomeView

---
+++ src/views/HomeView.vue
@@ -0,0 +1,226 @@
+<template>
+  <div class="home">
+    <div class="section-img">
+
+      <div class="container-img">
+        <div class="img"> </div>
+      </div>
+
+      <div class="content-description-img">
+        <h1 class="title-img">Tom Brady</h1>
+        <h3 class="description-img">Follow Tom on social media and check out the links below for his latest ventures.</h3>
+        <div class="icon-collection">
+
+          <a href="https://twitter.com/TomBrady" class="icon" target="_blank" rel="noopener noreferrer">
+            <v-icon class="icon-color">mdi-twitter</v-icon>
+          </a>
+          <a href="https://twitter.com/TomBrady" class="icon" target="_blank" rel="noopener noreferrer">
+            <v-icon class="icon-color">mdi-instagram</v-icon>
+          </a>
+          <a href="https://twitter.com/TomBrady" class="icon" target="_blank" rel="noopener noreferrer">
+            <svg class="icon-color-svg" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="facebook-f"
+              role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+              <path fill="currentColor"
+                d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z">
+              </path>
+            </svg>
+          </a>
+          <a href="https://twitter.com/TomBrady" class="icon" target="_blank" rel="noopener noreferrer">
+            <svg class="icon-color-svg" aria-hidden="true" focusable="false" data-prefix="fal" data-icon="podcast"
+              role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+              <path fill="currentColor"
+                d="M326.011 313.366a81.658 81.658 0 0 0-11.127-16.147c-1.855-2.1-1.913-5.215-.264-7.481C328.06 271.264 336 248.543 336 224c0-63.221-52.653-114.375-116.41-111.915-57.732 2.228-104.69 48.724-107.458 106.433-1.278 26.636 6.812 51.377 21.248 71.22 1.648 2.266 1.592 5.381-.263 7.481a81.609 81.609 0 0 0-11.126 16.145c-2.003 3.816-7.25 4.422-9.961 1.072C92.009 289.7 80 258.228 80 224c0-79.795 65.238-144.638 145.178-143.995 77.583.624 141.19 63.4 142.79 140.969.73 35.358-11.362 67.926-31.928 93.377-2.738 3.388-8.004 2.873-10.029-.985zM224 0C100.206 0 0 100.185 0 224c0 82.003 43.765 152.553 107.599 191.485 4.324 2.637 9.775-.93 9.078-5.945-1.244-8.944-2.312-17.741-3.111-26.038a6.025 6.025 0 0 0-2.461-4.291c-48.212-35.164-79.495-92.212-79.101-156.409.636-103.637 84.348-188.625 187.964-190.76C327.674 29.822 416 116.79 416 224c0 63.708-31.192 120.265-79.104 155.21a6.027 6.027 0 0 0-2.462 4.292c-.799 8.297-1.866 17.092-3.11 26.035-.698 5.015 4.753 8.584 9.075 5.947C403.607 376.922 448 306.75 448 224 448 100.204 347.814 0 224 0zm64 355.75c0 32.949-12.871 104.179-20.571 132.813C262.286 507.573 242.858 512 224 512c-18.857 0-38.286-4.427-43.428-23.438C172.927 460.134 160 388.898 160 355.75c0-35.156 31.142-43.75 64-43.75 32.858 0 64 8.594 64 43.75zm-32 0c0-16.317-64-16.3-64 0 0 27.677 11.48 93.805 19.01 122.747 6.038 2.017 19.948 2.016 25.981 0C244.513 449.601 256 383.437 256 355.75zM288 224c0 35.346-28.654 64-64 64s-64-28.654-64-64 28.654-64 64-64 64 28.654 64 64zm-32 0c0-17.645-14.355-32-32-32s-32 14.355-32 32 14.355 32 32 32 32-14.355 32-32z">
+              </path>
+            </svg>
+          </a>
+        </div>
+
+      </div>
+
+    </div>
+
+    <div class="img-responsive">
+
+      <div class="centered-images">
+
+        <v-row>
+          <v-col class="col-img" cols="6" sm="6" md="6" lg="6" xl="6">
+          
+              <img class="img-main" src="https://embed.filekitcdn.com/e/8RY3pynSkGMV9dpqgoW7P5/jzAn8LyU1rLw47fqrziL1B"
+                alt="">
+               
+           
+    <p>Shop TB12</p>
+
+          </v-col>    
+          <v-col class="col-img" cols="6" sm="6" md="6" lg="6" xl="6">
+            <img class="img-main" src="https://embed.filekitcdn.com/e/8RY3pynSkGMV9dpqgoW7P5/jzAn8LyU1rLw47fqrziL1B"
+              alt="">
+            <p>Autograph.io</p>
+
+          </v-col>
+
+          <v-col class="col-img" cols="6" sm="6" md="6" lg="6" xl="6">
+            <img class="img-main" src="https://embed.filekitcdn.com/e/8RY3pynSkGMV9dpqgoW7P5/jzAn8LyU1rLw47fqrziL1B"
+              alt="">
+              <p>BRADY | The Next Generation</p>
+          </v-col>
+          <v-col class="col-img" cols="6" sm="6" md="6" lg="6" xl="6">
+            <img class="img-main" src="https://embed.filekitcdn.com/e/8RY3pynSkGMV9dpqgoW7P5/jzAn8LyU1rLw47fqrziL1B"
+              alt="">
+              <p>Shadow Lion: Creative Studio</p>
+          </v-col>
+        </v-row>
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
+<style>
+.home {
+  background: linear-gradient(90deg, #d093f6 0, #5126d1f2) !important;
+  padding: 0 21% 0 21%;
+}
+
+
+
+
+.section-img {
+  display: flex;
+  justify-content: center;
+  padding-top: 100px;
+
+
+}
+
+.container-img {
+  padding-right: 50px;
+}
+
+.img {
+  border-radius: 10px !important;
+  height: 123px;
+  width: 155px;
+  background-image: url(../assets/brady.png);
+  background-size: contain;
+
+}
+
+.title-img {
+  font-weight: 800;
+}
+
+.description-img {
+  font-weight: 500 !important;
+  font-family: 'Work Sans', sans-serif;
+}
+
+.content-description-img {
+  text-align: start;
+  color: white;
+}
+
+.icon-collection {
+  display: flex;
+  padding-top: 20px;
+}
+
+.img-responsive {
+  padding-top: 120px;
+
+}
+
+.centered-images {
+  display: flex;
+  justify-content: center;
+
+}
+
+.img-main {
+    height: 70%;
+    width: 90%;
+    border-radius: 20px;
+  }
+
+.container-img-main{
+  display: flex;
+}
+.col-img {
+  display: inline;
+  justify-content: center;
+}
+
+.icon {
+  border-radius: 50px;
+  background-color: white;
+  height: 40px;
+  width: 40px;
+  align-items: center;
+  text-align: center;
+  padding-top: 7px;
+  margin-right: 10px;
+
+}
+
+.icon-color {
+  color: #6b3ad6 !important;
+  width: 20px;
+  height: 20px;
+
+}
+
+.icon-color-svg {
+  color: #6b3ad6 !important;
+  width: 20px;
+  height: 20px;
+  margin-top: 3px;
+
+}
+
+@media screen and (max-width: 600px) {
+  .home {
+    padding: 0 30px 0 30px;
+  }
+
+  .section-img {
+    display: inline;
+
+  }
+
+  .img {
+    height: 56px;
+    width: 70px;
+    background-size: cover;
+
+  }
+
+  .container-img {
+    padding-top: 40px;
+    padding-bottom: 40px;
+  }
+
+  .img-main {
+    height: 70%;
+    width: 90%;
+
+  }
+
+  .title-img {
+    padding-bottom: 20px;
+  }
+
+  .description-img {
+    font-size: 18px;
+  }
+
+}</style>
